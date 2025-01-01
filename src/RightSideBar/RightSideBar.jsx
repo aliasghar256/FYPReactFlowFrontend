91b9810e89317721b006e3629814be32c1da01d0
unknown <alih11737@gmail.com>
2025-01-01 23:32:53 +0500

    Playbook ID

--- src/RightSideBar/RightSideBar.jsx
+++ src/RightSideBar/RightSideBar.jsx
@@ -30,7 +30,7 @@ const RightSideBar = ({
     if (!selectedPlaybook) return;
 
     try {
-      const url = `http://93.127.202.133:3000/playbook/${selectedPlaybook.id}/context_logs`;
+      const url = `http://93.127.202.133:3000/playbook/${selectedPlaybook.id}/global_context_log`;
       const response = await axios.get(url);
       setContextLogs(response.data.logs || []);
     } catch (error) {
